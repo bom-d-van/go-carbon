934115bc4e0429861a68752badca9b28ba620519
bom-d-van <bom.d.van@gmail.com>
2019-06-27 17:44:44 +0200

    trie: debugging case [0-2][4-5a-z]

--- carbonserver/carbonserver.go
+++ carbonserver/carbonserver.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
+	"log"
 	"math"
 	"net"
 	"net/http"
@@ -716,12 +717,12 @@ func (listener *CarbonserverListener) expandGlobs(query string) ([]string, []boo
 	query = strings.Replace(query, ".", "/", -1)
 
 	var globs []string
-	if !strings.HasSuffix(query, "*") {
-		globs = append(globs, query+".wsp")
-		logger.Debug("appending file to globs struct",
-			zap.Strings("globs", globs),
-		)
-	}
+	// if !strings.HasSuffix(query, "*") {
+	// 	globs = append(globs, query+".wsp")
+	// 	logger.Debug("appending file to globs struct",
+	// 		zap.Strings("globs", globs),
+	// 	)
+	// }
 	globs = append(globs, query)
 	// TODO(dgryski): move this loop into its own function + add tests
 	for {
@@ -772,6 +773,7 @@ func (listener *CarbonserverListener) expandGlobs(query string) ([]string, []boo
 		var files []string
 		var leafs []bool
 		for _, g := range globs {
+			log.Printf("g = %+v\n", g)
 			f, l, err := fidx.trieIdx.search(g, math.MaxInt64)
 			if err != nil {
 				panic(err)
